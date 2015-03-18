c35aab911538e787ba89b39925c164ddaf9ecf70
Alexey Novikov <velocityzen@gmail.com>
2015-03-18 16:21:19 -0400

    new tool api

--- index.js
+++ index.js
@@ -1,35 +1,50 @@
 "use strict";
-let Coleccionista = require("coleccionista");
-
-let isArray = Array.isArray;
+let path = require("path");
+let glob = require("glob-all");
+let Transform = require("stream").Transform;
+let Material = require("fuller-material-file");
 
 let SrcFiles = function(fuller, options) {
 	fuller.bind(this);
 
-	this.dev = options.dev;
 	this.src = options.src;
+	this.dst = options.dst;
 };
 
-SrcFiles.prototype.build = function(stream, master) {
-	if(isArray(stream) || typeof stream === "string") {
+SrcFiles.prototype = {
+	build: function(src, dst) {
 		let self = this;
 
-		stream = new Coleccionista({
-				files: stream,
-				path: this.src
-			}, function(err, files) {
-				if(err) {
-					self.error(err.toString().replace("Error:", ""));
-				} else {
-					self.addDependence(files, master);
-				}
+		let next = new Transform({
+			objectMode: true,
+			transform: function(mat, enc, cb) {
+				cb(null, mat);
+			}
+		});
+
+		glob(src, {cwd: this.src}, function(err, files) {
+				// self.addDependencies(files, dst);
+				err && self.error(err);
+			})
+			.on("match", function(f) {
+				let file = path.join(self.src, f);
+				let newMat = new Material({ id: dst, path: file })
+					.dst(path.join(self.dst, f))
+					.error(function(err) {
+						self.error(err);
+					});
+				self.addDependencies(file, dst);
+				next.write(newMat);
+			})
+			.on("end", function() {
+				next.end();
 			})
 			.on("error", function(err) {
-				self.error(err.toString().replace("Error:", ""));
+				self.error(err);
 			});
-	}
 
-	return stream;
+		return next;
+	}
 };
 
 
--- readme.md
+++ readme.md
@@ -1,5 +1,6 @@
 # Multiple files source tool for Fuller #
 **Build everything with right tool**
+
 [For more info about fuller](https://github.com/fullerjs/fuller)
 
 You can use array of file name or wildcard string in you plan.

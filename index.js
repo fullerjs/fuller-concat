3eee8c8b825fd54ddc36777f231766ac0f32f9cb
velocityzen <velocityzen@gmail.com>
2016-10-14 23:58:12 -0400

    + editor config

--- index.js
+++ index.js
@@ -1,54 +1,52 @@
-"use strict";
-let path = require("path");
-let glob = require("glob-all");
-let Transform = require("stream").Transform;
-let Material = require("fuller-material-file");
+'use strict';
+let path = require('path');
+let glob = require('glob-all');
+let Transform = require('stream').Transform;
+let Material = require('fuller-material-file');
 
 let SrcFiles = function(fuller, options) {
-	fuller.bind(this);
-
-	this.src = options.src;
-	this.dst = options.dst;
+  fuller.bind(this);
+  this.src = options.src;
+  this.dst = options.dst;
 };
 
 SrcFiles.prototype = {
-	build: function(src, dst) {
-		let self = this;
+  build: function(src, dst) {
+    let self = this;
 
-		let next = new Transform({
-			objectMode: true,
-			transform: function(mat, enc, cb) {
-				cb(null, mat);
-			}
-		});
+    let next = new Transform({
+      objectMode: true,
+      transform: function(mat, enc, cb) {
+        cb(null, mat);
+      }
+    });
 
-		if(Array.isArray(src)) {
-			src = src.slice(0);
-		}
+    if (Array.isArray(src)) {
+      src = src.slice(0);
+    }
 
-		glob(src, {cwd: this.src}, function(err, files) {
-				// self.addDependencies(files, dst);
-				err && self.error(err);
-			})
-			.on("match", function(f) {
-				let file = path.join(self.src, f);
-				let newMat = new Material({ id: dst, path: file })
-					.dst(path.join(self.dst, f))
-					.error(function(err) {
-						self.error(err);
-					});
-				self.addDependencies(file, dst);
-				next.write(newMat);
-			})
-			.on("end", function() {
-				next.end();
-			})
-			.on("error", function(err) {
-				self.error(err);
-			});
+    glob(src, { cwd: this.src }, function(err) {
+      err && self.error(err);
+    })
+    .on('match', function(f) {
+      let file = path.join(self.src, f);
+      let newMat = new Material({ id: dst, path: file })
+        .dst(path.join(self.dst, f))
+        .error(function(err) {
+          self.error(err);
+        });
+      self.addDependencies(file, dst);
+      next.write(newMat);
+    })
+    .on('end', function() {
+      next.end();
+    })
+    .on('error', function(err) {
+      self.error(err);
+    });
 
-		return next;
-	}
+    return next;
+  }
 };
 
 
